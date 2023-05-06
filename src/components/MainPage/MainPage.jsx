e8caee9731e1d4e05733130718bb1c8b46a069fd
Nicolas Lopez Pineda <nicolas.pineda@correounivalle.edu.co>
2023-05-06 15:07:47 -0500

    Se agregaron los botones de Whatsapp y telefono tipo fixed

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -73,10 +73,10 @@ const MainPage = () => {
       </Grid>
       <Box sx={{ position: "fixed", bottom: 20, right: 3 }}>
         <ButtonGroup orientation="vertical">
-          <IconButton size="medium" sx={{ color: "white", bgcolor: "green", mb:1 }} component={ReactWhatsapp} number="57-312-237-7845" message="Por favor deje su mensaje. En un momento será atendido.">
+          <IconButton size="medium" sx={{ color: "white", bgcolor: "green", mb:1, "&:hover":{color: "white", bgcolor: "green"} }} component={ReactWhatsapp} number="57-312-237-7845" message="Por favor deje su mensaje. En un momento será atendido.">
             <WhatsApp sx={{ fontSize: 40 }} />
           </IconButton>
-          <IconButton size="medium" sx={{ color: "white", bgcolor: "#0063cc"}} href="tel:+573122377845" target={"_blank"}>
+          <IconButton size="medium" sx={{ color: "white", bgcolor: "#0063cc", "&:hover":{color: "white", bgcolor: "#0063cc"}}} href="tel:+573122377845" target={"_blank"}>
             <PhoneAndroidIcon sx={{ fontSize: 40 }} />
           </IconButton>
         </ButtonGroup>
